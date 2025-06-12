ea515562e167108f40f7b3b06cf55f886a9bd0a9
toyo <tyudosen@gmail.com>
2025-06-11 22:45:24 -0700

    comment out weather tool call

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -39,19 +39,19 @@ export async function POST(req: Request) {
 				}),
 				execute: async ({ question }) => findRelevantContent(question),
 			}),
-			weather: tool({
-				description: 'Get weather in a location (farenheit)',
-				parameters: z.object({
-					location: z.string().describe('The location to get the weather for')
-				}),
-				execute: async ({ location }) => {
-					const tempreature = Math.round(Math.random() * (90 - 32) + 32);
-					return {
-						location,
-						tempreature
-					}
-				}
-			}),
+			// weather: tool({
+			// 	description: 'Get weather in a location (farenheit)',
+			// 	parameters: z.object({
+			// 		location: z.string().describe('The location to get the weather for')
+			// 	}),
+			// 	execute: async ({ location }) => {
+			// 		const tempreature = Math.round(Math.random() * (90 - 32) + 32);
+			// 		return {
+			// 			location,
+			// 			tempreature
+			// 		}
+			// 	}
+			// }),
 			convertFarenheitToCelcius: tool({
 				description: 'Converts temperature in farenheit to celsius',
 				parameters: z.object({
